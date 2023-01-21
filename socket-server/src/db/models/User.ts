71e3d63051a9db91f91a89343c196117f6c73b59
wiktor-falek <falekwiktor@gmail.com>
2023-01-21 23:07:30 +0100

    Remove redundant method.

--- socket-server/src/db/models/User.ts
+++ socket-server/src/db/models/User.ts
@@ -14,53 +14,6 @@ class User {
     });
     return user;
   }
-
-  async findCurrentCharacter(
-    username: string,
-    sessionId: string
-  ): Promise<any | null> {
-    
-    const user = await this.collection.findOne(
-      {
-        "account.username": username,
-        "account.sessionId": sessionId,
-      },
-      { projection: { "activity.currentCharacter": 1 } }
-    );
-    if (user === null) return null;
-
-    const currentCharacterId = user.activity.currentCharacter;
-
-    const currentCharacter = await this.collection
-      .find({ _id: user._id })
-      .project({ characters: { $elemMatch: { _id: currentCharacterId } } })
-      .toArray();
-
-    if (currentCharacter === null) return null;
-
-    return currentCharacter[0].characters[0];
-
-    // THIS WOULD NEED TO FILTER USER BY _id FIRST TO WORK
-    // This is probably the best way to do this, if I can figure out how to filter
-    // const currentCharacter = await this.collection
-    //   .aggregate([
-    //     {
-    //       $project: {
-    //         result: {
-    //           $filter: {
-    //             input: "$characters",
-    //             as: "character",
-    //             cond: {
-    //               $eq: ["$activity.currentCharacter", "$$character._id"],
-    //             },
-    //           },
-    //         },
-    //       },
-    //     },
-    //   ])
-    //   .toArray();
-    // return currentCharacter[0].result[0];
-  }
 }
 
 export default new User();
