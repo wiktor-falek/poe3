76239d77864d47a5fe451cc21f8a4927073e425a
wiktor-falek <falekwiktor@gmail.com>
2023-02-13 21:50:41 +0100

    Use starting items from items module on character creation.

--- api/src/globals/playerClasses.js
+++ api/src/globals/playerClasses.js
@@ -1,3 +1,10 @@
+import {
+  SWORDSMAN_STARTING_ITEMS,
+  RANGER_STARTING_ITEMS,
+  SORCERER_STARTING_ITEMS,
+  ASSASSIN_STARTING_ITEMS,
+} from "items";
+
 export const startingAttributes = {
   swordsman: {
     strength: 8,
@@ -34,10 +41,10 @@ export const startingAttributes = {
 
 export const startingGear = {
   swordsman: {
-    hand: null,
+    hand: SWORDSMAN_STARTING_ITEMS.hand,
     offhand: null,
     helmet: null,
-    chest: null,
+    chest: SWORDSMAN_STARTING_ITEMS.chest,
     gloves: null,
     boots: null,
     ring_1: null,
@@ -47,110 +54,23 @@ export const startingGear = {
   },
 
   ranger: {
-    hand: {
-      base: "Short Bow",
-      type: "weapon",
-      rarity: "normal",
-      requirements: {
-        level: 1,
-      },
-      baseMods: [
-        {
-          modId: "base_crit_chance",
-          values: [5],
-          description: "Critical Strike Chance: #",
-        },
-        {
-          modId: "base_phys_dmg",
-          values: [2, 4],
-          description: "Physical damage: # to #",
-        },
-      ],
-    },
+    hand: RANGER_STARTING_ITEMS.hand,
     offhand: null,
     helmet: null,
-    chest: null,
+    chest: RANGER_STARTING_ITEMS.chest,
     gloves: null,
     boots: null,
-    ring_1: {
-      base: "Gold Ring",
-      type: "ring",
-      rarity: "unique",
-      name: "The One Ring",
-      description:
-        "One ring to rule them all, one ring to find them, One ring to bring them all, and in the darkness bind them; In the Land of Mordor where the shadows lie.",
-      requirements: {
-        level: 1,
-      },
-      implicits: [
-        {
-          modId: "to_life",
-          description: "+# to Life",
-          values: [5],
-        },
-      ],
-      affixes: [
-        { modId: "to_strength", description: "+# to Strength", values: [1] },
-        { modId: "to_dexterity", description: "+# to Dexterity", values: [1] },
-        {
-          modId: "to_intelligence",
-          description: "+# to Intelligence",
-          values: [1],
-        },
-        { modId: "to_vitality", description: "+# to Vitality", values: [1] },
-        { modId: "to_speed", description: "+# to Speed", values: [1] },
-      ],
-    },
-    ring_2: {
-      base: "Sapphire Ring",
-      type: "ring",
-      rarity: "rare",
-      requirements: {
-        level: 5,
-      },
-      implicits: [
-        {
-          modId: "to_mana",
-          description: "+# to Mana",
-          values: [10],
-        },
-      ],
-      affixes: {
-        prefixes: [
-          {
-            modId: "to_life",
-            description: "+# to Life",
-            values: [1],
-          },
-          {
-            modId: "to_mana",
-            description: "+# to Mana",
-            values: [2],
-          },
-          {
-            modId: "physical_damage_to_attacks",
-            description: "Adds # to # Physical Damage to Attacks",
-            values: [1, 2],
-          },
-        ],
-        suffixes: [
-          {
-            modId: "to_intelligence",
-            description: "+# to Intelligence",
-            values: [2],
-          },
-        ],
-      },
-    },
+    ring_1: null,
+    ring_2: null,
     amulet: null,
     belt: null,
   },
 
   sorcerer: {
-    hand: null,
+    hand: SORCERER_STARTING_ITEMS.hand,
     offhand: null,
     helmet: null,
-    chest: null,
+    chest: SORCERER_STARTING_ITEMS.chest,
     gloves: null,
     boots: null,
     ring_1: null,
@@ -160,10 +80,10 @@ export const startingGear = {
   },
 
   assassin: {
-    hand: null,
+    hand: ASSASSIN_STARTING_ITEMS.hand,
     offhand: null,
     helmet: null,
-    chest: null,
+    chest: ASSASSIN_STARTING_ITEMS.chest,
     gloves: null,
     boots: null,
     ring_1: null,
