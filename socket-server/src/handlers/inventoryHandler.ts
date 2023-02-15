3d13db7f25eeca0468ce10ed53b31eb0ab9475ee
wiktor-falek <falekwiktor@gmail.com>
2023-02-15 14:42:32 +0100

    Hardcode array of 3 items.

--- socket-server/src/handlers/inventoryHandler.ts
+++ socket-server/src/handlers/inventoryHandler.ts
@@ -1,5 +1,6 @@
 import type Client from "../helpers/Client";
 import type { Socket } from "socket.io";
+import { choice } from "pyrand";
 
 function registerInventoryHandler(
   io: any,
@@ -7,37 +8,93 @@ function registerInventoryHandler(
   client: Client
 ): void {
   const addTestItem = () => {
-    const item = {
-      name: "Gold Ring",
-      slot: "ring",
-      rarity: "magic",
-      requirements: {
-        level: 5,
-      },
-      implicits: [
-        {
-          modId: "to_life",
-          description: "+# to Life",
-          values: [5],
+    const items = [
+      {
+        name: "Gold Ring",
+        slot: "ring",
+        rarity: "magic",
+        requirements: {
+          level: 5,
         },
-      ],
-      affixes: {
-        prefixes: [
+        implicits: [
           {
             modId: "to_life",
             description: "+# to Life",
-            values: [2],
+            values: [5],
           },
         ],
-        suffixes: [
+        affixes: {
+          prefixes: [
+            {
+              modId: "to_life",
+              description: "+# to Life",
+              values: [2],
+            },
+          ],
+          suffixes: [
+            {
+              modId: "to_dexterity",
+              description: "+# to Dexterity",
+              values: [2],
+            },
+          ],
+        },
+      },
+
+      {
+        name: "Sapphire Ring",
+        slot: "ring",
+        rarity: "normal",
+        requirements: {
+          level: 5,
+        },
+        implicits: [
+          {
+            modId: "to_mana",
+            description: "+# to Mana",
+            values: [5],
+          },
+        ],
+        affixes: {
+          prefixes: [],
+          suffixes: [],
+        },
+      },
+      {
+        base: "Gold Ring",
+        slot: "ring",
+        rarity: "unique",
+        name: "The One Ring",
+        description:
+          "One ring to rule them all, one ring to find them, One ring to bring them all, and in the darkness bind them; In the Land of Mordor where the shadows lie.",
+        requirements: {
+          level: 1,
+        },
+        implicits: [
+          {
+            modId: "to_life",
+            description: "+# to Life",
+            values: [5],
+          },
+        ],
+        affixes: [
+          { modId: "to_strength", description: "+# to Strength", values: [1] },
           {
             modId: "to_dexterity",
             description: "+# to Dexterity",
-            values: [2],
+            values: [1],
           },
+          {
+            modId: "to_intelligence",
+            description: "+# to Intelligence",
+            values: [1],
+          },
+          { modId: "to_vitality", description: "+# to Vitality", values: [1] },
+          { modId: "to_speed", description: "+# to Speed", values: [1] },
         ],
       },
-    };
+    ];
+    const item = choice(items);
 
     client.characterModelProxy.addItem(item).then((result) => {
       if (!result.ok) {
@@ -48,16 +105,20 @@ function registerInventoryHandler(
     });
   };
 
-  const swapInventoryIncides = () => {
-    client.characterModelProxy.swapInventoryIncides(0, 1).then((result) => {
-      console.log(result);
-      if (!result.ok) {
-        return socket.emit("error", result.reason)
-      }
-      socket.emit("inventory:swap-inventory-indices", {
-        swappedIndices: result.swappedIndices,
+  const swapInventoryIncides = (firstIndex: any, secondIndex: any) => {
+    if (typeof firstIndex !== "number" || typeof secondIndex !== "number") {
+      return socket.emit("error", "Invalid parameters");
+    }
+    client.characterModelProxy
+      .swapInventoryIncides(firstIndex, secondIndex)
+      .then((result) => {
+        if (!result.ok) {
+          return socket.emit("error", result.reason ?? "Action failed");
+        }
+        socket.emit("inventory:swap-inventory-indices", {
+          swappedIndices: result.swappedIndices,
+        });
       });
-    });
   };
 
   socket.on("inventory:add-test-item", addTestItem);
