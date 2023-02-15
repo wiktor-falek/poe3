897702ade1e0c786e158e8938b301d8130988ac4
wiktor-falek <falekwiktor@gmail.com>
2023-02-15 12:59:12 +0100

    Change default hp to 20.

--- api/src/db/schemas/characterSchema.js
+++ api/src/db/schemas/characterSchema.js
@@ -31,7 +31,7 @@ const characterSchema = Joi.object({
     maxHp: Joi.number()
       .integer()
       .min(0)
-      .default(10),
+      .default(20),
     mp: Joi.number()
       .integer()
       .min(0)
