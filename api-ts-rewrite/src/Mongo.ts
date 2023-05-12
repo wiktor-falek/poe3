5c2d0d46ec8e4d3f70353c6ff978c1670902b434
wiktor-falek <falekwiktor@gmail.com>
2023-05-12 04:15:40 +0200

    Remove unnecessary method

--- api-ts-rewrite/src/Mongo.ts
+++ api-ts-rewrite/src/Mongo.ts
@@ -25,23 +25,6 @@ class Mongo {
     }
     return this.client;
   }
-
-  static async connect() {
-    try {
-      // Connect the client to the server (optional starting in v4.7)
-      await this.client.connect();
-      // Send a ping to confirm a successful connection
-      await this.client.db("admin").command({ ping: 1 });
-      console.log(
-        "Pinged your deployment. You successfully connected to MongoDB!"
-      );
-    } catch {
-      console.log("Connection to database failed. Is the uri correct?");
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await this.client.close();
-    }
-  }
 }
 
 export default Mongo;
