ffd23eb0f13ceba716209d0fa9cfd17b11bbe900
wiktor-falek <falekwiktor@gmail.com>
2023-05-17 03:58:27 +0200

    add /play route, fetch all characters before mount

--- client-refactor/src/router.ts
+++ client-refactor/src/router.ts
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "./views/Home.vue";
 import Signin from "./views/Signin.vue";
+import Play from "./views/Play.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", name: "home", component: Home },
     { path: "/signin", name: "signin", component: Signin },
+    { path: "/play", name: "play", component: Play },
   ],
 });
 
---
+++ client-refactor/src/views/Play.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import { onBeforeMount } from "vue";
+import type { ResponseGetAllCharactersOverview } from "../../../common/api-types/index";
+import { Ref } from "vue";
+
+const allCharacters: Ref<ResponseGetAllCharactersOverview> = ref([]);
+
+onBeforeMount(async () => {
+  const response = await fetch("http://localhost:3000/api/characters", {
+    method: "GET",
+    credentials: "include",
+  });
+
+  console.log(response.status);
+});
+</script>
+
+<template>
+  <h1>Play</h1>
+</template>
