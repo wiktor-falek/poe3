6c41eb520670ee9397dde4059d19f492c99b9fa9
wiktor-falek <falekwiktor@gmail.com>
2023-05-19 22:46:19 +0200

    implement character creation, add css variables, change font

---
+++ client-refactor/src/components/Global/Attributes.vue
@@ -0,0 +1,109 @@
+<script lang="ts" setup>
+import type { Attributes } from "../../../../common/types";
+const props = defineProps<{ attributes: Attributes }>();
+
+console.log(props.attributes);
+</script>
+
+<template>
+  <div class="attributes">
+    <div class="attribute-bar">
+      <p>STR</p>
+      <div
+        class="attribute"
+        v-for="index in 8"
+        :class="{ 'attribute--strength': attributes.strength >= index }"
+      >
+      </div>
+      <p>{{ attributes.strength }}/8</p>
+    </div>
+
+    <div class="attribute-bar">
+      <p>DEX</p>
+      <div
+        class="attribute"
+        v-for="index in 8"
+        :class="{ 'attribute--dexterity': attributes.dexterity >= index }"
+      ></div>
+      <p>{{ attributes.dexterity }}/8</p>
+    </div>
+
+    <div class="attribute-bar">
+      <p>INT</p>
+      <div
+        class="attribute"
+        v-for="index in 8"
+        :class="{ 'attribute--intelligence': attributes.intelligence >= index }"
+      ></div>
+      <p>{{ attributes.intelligence }}/8</p>
+    </div>
+
+    <div class="attribute-bar">
+      <p>VIT</p>
+      <div
+        class="attribute"
+        v-for="index in 8"
+        :class="{ 'attribute--vitality': attributes.vitality >= index }"
+      ></div>
+      <p>{{ attributes.vitality }}/8</p>
+    </div>
+
+    <div class="attribute-bar">
+      <p>SPD</p>
+      <div
+        class="attribute"
+        v-for="index in 8"
+        :class="{ 'attribute--speed': attributes.speed >= index }"
+      ></div>
+      <p>{{ attributes.speed }}/8</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.attributes {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+
+.attribute-bar {
+  display: flex;
+  gap: 5px;
+  align-items: center;
+}
+
+.attribute-bar p {
+  margin: 0;
+  line-height: 1em;
+  padding-top: 1.5px;
+}
+
+.attribute {
+  height: 16px;
+  width: 16px;
+  border-radius: 50%;
+  display: inline-block;
+  background-color: gray;
+}
+
+.attribute--strength {
+  background-color: var(--attribute-strength);
+}
+
+.attribute--dexterity {
+  background-color: var(--attribute-dexterity);
+}
+
+.attribute--intelligence {
+  background-color: var(--attribute-intelligence);
+}
+
+.attribute--vitality {
+  background-color: var(--attribute-vitality);
+}
+
+.attribute--speed {
+  background-color: var(--attribute-speed);
+}
+</style>
--- client-refactor/src/views/Creation.vue
+++ client-refactor/src/views/Creation.vue
@@ -1,31 +1,117 @@
 <script lang="ts" setup>
 import { Ref } from "vue";
-import type { CharacterClass } from "../../../common/types/index";
+import type { CharacterClass, Attributes } from "../../../common/types/index";
 import { ref } from "vue";
+import AttributesVue from "../components/Global/Attributes.vue";
+import router from "../router";
 
 interface BannerData {
   name: CharacterClass;
-  icon: string;
+  backgroundUrl: string;
 }
 
 const bannersData: Array<BannerData> = [
-  { name: "swordsman", icon: "" },
-  { name: "ranger", icon: "" },
-  { name: "sorcerer", icon: "" },
-  { name: "assassin", icon: "" },
+  { name: "swordsman", backgroundUrl: "" },
+  { name: "ranger", backgroundUrl: "" },
+  { name: "sorcerer", backgroundUrl: "" },
+  { name: "assassin", backgroundUrl: "" },
 ];
 
+interface ClassData {
+  quote: string;
+  description: string;
+  attributes: Attributes;
+}
+
+const classesData: { [key in CharacterClass]: ClassData } = {
+  swordsman: {
+    quote: "Swordsman quote dolor sit amet.",
+    description:
+      "Description ipsum dolor sit amet consectetur adipisicing elit. Quo eum omnis quasi tempore odit nesciunt, cum laboriosam nam quae est!",
+    attributes: {
+      strength: 8,
+      dexterity: 6,
+      intelligence: 5,
+      vitality: 8,
+      speed: 5,
+    },
+  },
+  ranger: {
+    quote: "Ranger quote dolor sit amet.",
+    description:
+      "Description ipsum dolor sit amet consectetur adipisicing elit. Quo eum omnis quasi tempore odit nesciunt, cum laboriosam nam quae est!",
+    attributes: {
+      strength: 6,
+      dexterity: 8,
+      intelligence: 5,
+      vitality: 6,
+      speed: 7,
+    },
+  },
+  sorcerer: {
+    quote: "Sorcerer quote dolor sit amet.",
+    description:
+      "Description ipsum dolor sit amet consectetur adipisicing elit. Quo eum omnis quasi tempore odit nesciunt, cum laboriosam nam quae est!",
+    attributes: {
+      strength: 6,
+      dexterity: 6,
+      intelligence: 8,
+      vitality: 5,
+      speed: 7,
+    },
+  },
+  assassin: {
+    quote: "Assassin quote dolor sit amet.",
+    description:
+      "Description ipsum dolor sit amet consectetur adipisicing elit. Quo eum omnis quasi tempore odit nesciunt, cum laboriosam nam quae est!",
+    attributes: {
+      strength: 5,
+      dexterity: 7,
+      intelligence: 7,
+      vitality: 5,
+      speed: 8,
+    },
+  },
+};
+
 const selectedClass: Ref<CharacterClass | null> = ref(null);
+const characterName: Ref<string | null> = ref(null);
 
-// TODO: @click select button show details of the selected class
-// const selectedBannerDetail = ref(null);
+async function createCharacter(e: Event) {
+  e.preventDefault();
 
-function createCharacter() {
-  fetch("http://localhost:3000/api/characters", {
+  if (selectedClass.value === null || characterName.value == null) {
+    // TODO: display error
+    return;
+  }
+
+  if (characterName.value.length < 3) {
+    // TODO: display error
+    return;
+  }
+  if (characterName.value.length > 24) {
+    // TODO: display error
+    return;
+  }
+
+  const response = await fetch("http://localhost:3000/api/characters", {
     method: "POST",
     credentials: "include",
-    body: new URLSearchParams({}),
+    body: new URLSearchParams({
+      class: selectedClass.value,
+      name: characterName.value,
+    }),
   });
+
+  const result = await response.json();
+
+  if (!response.ok) {
+    // TODO: display error
+    // const error = result.error;
+    return;
+  }
+
+  router.push("/play")
 }
 </script>
 
@@ -33,31 +119,66 @@ function createCharacter() {
   <main>
     <h1>Choose your class</h1>
     <div class="wrapper">
-      <div class="character-banners">
+      <div v-if="selectedClass === null" class="character-banners">
         <div
+          tabindex="0"
           class="banner"
           v-for="banner in bannersData"
           @click="selectedClass = banner.name"
-          :class="{ selected: selectedClass === banner.name }"
+          @keyup.enter="selectedClass = banner.name"
+          @keyup.space="selectedClass = banner.name"
         >
           <p class="banner__name">{{ banner.name }}</p>
         </div>
       </div>
 
-      <div class="menu">
+      <div v-else class="character-detail">
+        <button @click="selectedClass = null" class="button">Go Back</button>
+        <h1 class="character-detail__quote">
+          {{ classesData[selectedClass].quote }}
+        </h1>
+
+        <p class="character-detail__description">
+          {{ classesData[selectedClass].description }}
+        </p>
+
+        <div class="character-detail__attributes">
+          <div class="character-detail__attributes--strength">
+            <AttributesVue
+              :attributes="classesData[selectedClass].attributes"
+            />
+          </div>
+        </div>
+      </div>
+
+      <form
+        class="menu"
+        id="create-character"
+        action="POST"
+        @submit="createCharacter($event)"
+      >
         <div class="menu__top">
           <RouterLink to="/play" class="button">&lt;</RouterLink>
-          <input id="character-name" type="text" placeholder="Character Name" />
+          <input
+            id="character-name"
+            for="create-character"
+            type="text"
+            minlength="3"
+            maxlength="24"
+            required
+            placeholder="Character Name"
+            v-model="characterName"
+          />
           <!-- hidden element -->
           <a class="button flex-hide"></a>
         </div>
 
         <div class="menu__bottom">
-          <button id="select" :disabled="selectedClass === null" @click="">
+          <button type="submit" id="create" :disabled="selectedClass === null">
             Create
           </button>
         </div>
-      </div>
+      </form>
     </div>
   </main>
 </template>
@@ -65,7 +186,7 @@ function createCharacter() {
 <style scoped>
 main {
   padding: 5px;
-  max-width: 1000px;
+  max-width: 900px;
   margin: 0 auto;
 }
 
@@ -80,15 +201,28 @@ h1 {
   align-items: center;
   justify-content: center;
 }
+
+.character-detail {
+  display: flex;
+  flex-direction: column;
+  height: 500px;
+  box-sizing: border-box;
+  min-height: 200px;
+  border: 2px solid rgba(255, 255, 255, 0.2);
+  border-radius: 10px;
+  padding: 20px;
+}
+
 .banner {
   flex: 1;
   display: flex;
   justify-content: center;
   align-items: flex-end;
   border: 2px solid white;
-  min-width: 50px;
-  max-width: 230px;
-  aspect-ratio: 3.5 / 10;
+  min-height: 200px;
+  max-height: 500px;
+  box-sizing: border-box;
+  aspect-ratio: 1 / 3.5;
   border: 2px solid rgba(255, 255, 255, 0.2);
   border-radius: 10px;
   cursor: pointer;
@@ -97,7 +231,13 @@ h1 {
 .banner__name {
   font-weight: bold;
   text-transform: capitalize;
-  font-size: 0.6rem;
+  font-size: 0.4rem;
+}
+
+@media only screen and (min-width: 400px) {
+  .banner__name {
+    font-size: 0.8rem;
+  }
 }
 
 @media only screen and (min-width: 500px) {
@@ -146,7 +286,7 @@ h1 {
   height: 50px;
 }
 
-#select {
+#create {
   width: 100%;
   max-width: 200px;
 }
