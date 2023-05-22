81d4667f8eac14b864e6b1692e3873c979b0d2b9
wiktor-falek <falekwiktor@gmail.com>
2023-05-23 00:55:43 +0200

    add characterName property

--- client-refactor/src/stores/userStore.ts
+++ client-refactor/src/stores/userStore.ts
@@ -4,14 +4,17 @@ import { ref, watch } from "vue";
 export const useUserStore = defineStore("user", () => {
   const authenticated = ref(false);
   const username = ref("");
+  const characterName = ref("");
 
   // load persisted state from localStorage
   if (localStorage.getItem("authenticated") === "true") {
     authenticated.value = true;
   }
-
   if (localStorage.getItem("username")) {
     username.value = localStorage.getItem("username") ?? "";
+  }
+  if (localStorage.getItem("characterName")) {
+    characterName.value = localStorage.getItem("characterName") ?? "";
   }
 
   function setSignedIn(data: { authenticated: boolean; username: string }) {
@@ -32,9 +35,14 @@ export const useUserStore = defineStore("user", () => {
     localStorage.setItem("username", username.value);
   });
 
+  watch(characterName, () => {
+    localStorage.setItem("characterName", characterName.value);
+  });
+
   return {
     authenticated,
     username,
+    characterName,
     setSignedIn,
     setSignedOut,
   };
