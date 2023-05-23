45aa410b24701394052e85fd8b7d82cae98cce2e
wiktor-falek <falekwiktor@gmail.com>
2023-05-23 21:58:32 +0200

    implement joining global room with proper input validation

--- game-server/src/handlers/chatHandler.ts
+++ game-server/src/handlers/chatHandler.ts
@@ -1,8 +1,25 @@
+import Joi from "joi";
 import type { ChatNamespace, ChatSocket } from "..";
+import { ServerMessage } from "../components/message";
 
 function registerChatHandler(io: ChatNamespace, socket: ChatSocket) {
-  const join = (room: number) => {
-    console.log("someone joined chat");
+  const join = (_room: number) => {
+    const schema = Joi.number().required().min(1).max(1000);
+    const result = schema.validate(_room);
+    if (result.error) {
+      return socket.emit(
+        "message",
+        new ServerMessage("Invalid argument 'room'")
+      );
+    }
+
+    const room = result.value;
+
+    const roomName = `global:${room}`;
+
+    socket.join(roomName);
+
+    socket.emit("message", new ServerMessage(`You joined global room ${room}`));
   };
 
   socket.on("join", join);
