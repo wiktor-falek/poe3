350e8bba2e625cbdc54e1e45aff13d0389574a95
wiktor-falek <falekwiktor@gmail.com>
2023-05-23 18:27:36 +0200

    initialize game namespace connection

---
+++ client-refactor/src/socket/game.ts
@@ -0,0 +1,35 @@
+import { reactive } from "vue";
+import { io, Socket } from "socket.io-client";
+import type {
+  GameServerToClientEvents,
+  GameClientToServerEvents,
+} from "../../../common/events/gameServerEvents";
+
+const sessionId = document.cookie
+  .split("; ")
+  .find((row) => row.startsWith("sessionId="))
+  ?.split("=")[1];
+
+export const state = reactive({
+  connected: false,
+  fooEvents: [],
+  barEvents: [],
+});
+
+export const socket: Socket<
+  GameServerToClientEvents,
+  GameClientToServerEvents
+> = io("http://localhost:4000/game", {
+  autoConnect: false,
+  auth: {
+    sessionId,
+  },
+});
+
+socket.on("connect", () => {
+  state.connected = true;
+});
+
+socket.on("disconnect", () => {
+  state.connected = false;
+});
