392b65bf61a5fb0cc33331616c2f0324646d726d
wiktor-falek <falekwiktor@gmail.com>
2023-06-02 22:46:44 +0200

    rename client-refactor -> client & client -> old-client

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,16 +1,18 @@
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 import App from "./App.vue";
+import { createPinia } from "pinia";
+import "./style.css";
+import "./styles/form.css";
+import "./styles/components.css";
 import router from "./router";
-import vueClickOutsideElement from "vue-click-outside-element";
-
-import "./assets/main.css";
+import VueCookies from "vue-cookies";
 
 const pinia = createPinia();
+
 const app = createApp(App);
 
 app.use(pinia);
 app.use(router);
-app.use(vueClickOutsideElement);
+app.use(VueCookies);
 
 app.mount("#app");
