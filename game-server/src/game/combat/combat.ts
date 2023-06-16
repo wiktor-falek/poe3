fa08e4dee57ccd5ad01c1a664aa855b82d501ea0
wiktor-falek <falekwiktor@gmail.com>
2023-06-16 14:04:08 +0200

    combat class interface

--- game-server/src/game/combat/combat.ts
+++ game-server/src/game/combat/combat.ts
@@ -1,9 +1,24 @@
 class Combat {
   constructor() {
-    
+    // turn
+    // actionQueue
+  }
+
+  playerAction() {
+    // take action input - ability and target
+    // update state like subtracting health of the target
+    // validate and return action / error
   }
 
   continue() {
-    // keep calling next / starting new turn until player action is required
+    // keep executing actions for each enemy in the turn
+    // create next turn if turn.current === null
+    // keep going until either combat ends or awaiting player action
+  }
+
+  private enemyAction() {
+    // enemy ai choosing ability and targets
+    // updating state
+    // pushing action to actionQueue
   }
 }
