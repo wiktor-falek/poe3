73990729f87f42fbe2cd364f00384c03f7ac232a
wiktor-falek <falekwiktor@gmail.com>
2023-06-17 02:42:26 +0200

    rough idea of skill class

---
+++ game-server/src/game/skills/skill.ts
@@ -0,0 +1,78 @@
+type AbilityType = "attack" | "skill";
+
+type AbilityTarget = "target" | "around_target" | "random" | "all";
+
+interface EachLevel<T> {
+  1: T;
+  2: T;
+  3: T;
+  4: T;
+  5: T;
+  6: T;
+  7: T;
+  8: T;
+  9: T;
+  10: T;
+}
+
+interface AbilityDamage {
+  addedFlat?: number;
+  addedFlatEachLevel?: EachLevel<number>;
+  weaponDamageMultiplier?: number;
+  weaponDamageMultiplierEachLevel?: EachLevel<number>;
+}
+
+interface AbilityCost {
+  mp?: { base: number; levelIncrement: number };
+  hp?: { base: number; levelIncrement: number };
+  ap?: number;
+}
+
+class Ability {
+  static MAX_LEVEL: 10;
+  constructor(
+    public name: string,
+    public type: AbilityType,
+    public target: AbilityTarget,
+    public damage: AbilityDamage,
+    public cost?: AbilityCost
+  ) {
+    this.name = name;
+    this.type = type;
+    this.target = target;
+    this.cost = cost;
+  }
+
+  level() {
+    // calculate properties that vary with skill level
+    return { ...this, cost: {}, damage: {} };
+  }
+}
+
+const ability = new Ability(
+  "Heavy Strike",
+  "attack",
+  "target",
+  {
+    addedFlatEachLevel: {
+      1: 2,
+      2: 4,
+      3: 7,
+      4: 12,
+      5: 18,
+      6: 26,
+      7: 37,
+      8: 52,
+      9: 70,
+      10: 95,
+    },
+  },
+  {
+    mp: {
+      base: 5,
+      levelIncrement: 2,
+    },
+    ap: 2,
+  }
+);
+console.log(ability);
