44725bead74d3a584b5c0fc17e7e5f29b18cd1c9
wiktor-falek <falekwiktor@gmail.com>
2023-06-21 23:12:48 +0200

    inventory queries (wip)

--- game-server/src/db/models/character.ts
+++ game-server/src/db/models/character.ts
@@ -1,5 +1,7 @@
-import { StaticCharacter } from "../../../../common/index.js";
+import { ObjectId } from "mongodb";
+import { StaticCharacter, EquipmentSlot } from "../../../../common/index.js";
 import Mongo from "../mongo.js";
+import { Err, Ok } from "resultat";
 
 class Character {
   private static db = Mongo.getClient().db("game");
@@ -12,11 +14,34 @@ class Character {
 
   static async addSilver(name: string, amount: number) {
     const result = await this.collection.updateOne({ name }, { $inc: { silver: amount } });
+    if (result.modifiedCount === 0) {
+      return Err("Failed to award silver");
+    }
+    return Ok(1);
   }
 
-  static async deleteItem(name: string, id: string) {
-    const reuslt = await this.collection.updateOne({ name }, { $set: {  } });
+  static async deleteItem(name: string, id: ObjectId) {
+    const result = await this.collection.updateOne(
+      { name, "inventory.$._id": id },
+      {
+        $set: {
+          "inventory.$": undefined,
+        },
+      }
+    );
+
+    if (result.modifiedCount === 0) {
+      return Err("Failed to delete item");
+    }
+
+    return Ok(1);
   }
+
+  static async unequipItem(name: string, equipmentSlot: EquipmentSlot) {
+    // WAITING FOR HELP: https://www.mongodb.com/community/forums/c/data/6
+  }
+
+  static async equipItem(name: string, id: ObjectId) {}
 }
 
 export default Character;
