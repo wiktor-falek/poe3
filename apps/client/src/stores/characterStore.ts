ce0db94870b3b843f25fec6174899b844012687e
wiktor-falek <falekwiktor@gmail.com>
2023-06-26 18:26:51 +0200

    update type import path

--- apps/client/src/stores/characterStore.ts
+++ apps/client/src/stores/characterStore.ts
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia";
 import { Ref, ref, watch } from "vue";
-import type { DynamicCharacter, StaticCharacter } from "../../../common/index";
+import type { DynamicCharacter, StaticCharacter } from "../../../common/types/index";
 import getDynamicCharacter from "../../../common/dist/getDynamicCharacter";
 
 const useCharacterStore = defineStore("character", () => {
