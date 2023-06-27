eaeb470e74d692e752bfd89801d96e0f5311cae3
wiktor-falek <falekwiktor@gmail.com>
2023-06-27 13:20:48 +0200

    start refactoring state updates

--- apps/client/src/socket/gameServer.ts
+++ apps/client/src/socket/gameServer.ts
@@ -10,6 +10,8 @@ import type {
   ClientToServerEvents,
 } from "../../../common/types/gameServerEvents";
 import type Instance from "../../../game-server/src/game/instance/instance";
+import Queue from "../utils/queue";
+import { StateUpdate } from "../../../game-server/src/game/rooms/combatRoom";
 
 interface State {
   connected: boolean;
@@ -17,6 +19,7 @@ interface State {
   lobbies: { [lobbyId: string]: LobbyData };
   lobby: MembersOnlyLobbyData | null;
   instance: Instance | null;
+  instanceActionsQueue: Queue<StateUpdate>;
 }
 
 export const state: State = reactive({
@@ -25,6 +28,7 @@ export const state: State = reactive({
   lobbies: {},
   lobby: null,
   instance: null,
+  instanceActionsQueue: new Queue(),
 });
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
@@ -80,5 +84,64 @@ socket.on("lobby:delete", (lobbyId) => {
 });
 
 socket.on("instance:set", (instance) => {
+  state.instanceActionsQueue.items = [];
   state.instance = instance;
+});
+
+socket.on("instance:state-update", async (state) => {
+  // TODO: push state to instanceActionsQueue
+
+
+  // TODO: move this to a setInterval callback and try to dequeue
+  // const room = gameServer.state.instance!.room!;
+  // // take all updates from the state.actions and update state incrementally with a delay
+  // for (const action of state.actions) {
+  //   const target = room.players.find((player) => player.id === action.targetId);
+  //   if (target) {
+  //     displayDamagePopup(action.attackerId, action.targetId, action.damage, action.critical);
+  //     target.resources.hp = Math.max(target.resources.hp - action.damage, 0);
+  //   }
+  //   await new Promise((resolve) => setTimeout(resolve, 1000));
+  // }
+  // const { turnStartUpdate } = state;
+  // if (turnStartUpdate) {
+  //   const { playerId, resources } = turnStartUpdate;
+  //   const player = room.players.find((player) => player.id === playerId);
+  //   if (player) {
+  //     if (resources) {
+  //       if (resources.ap) {
+  //         player.resources.ap = resources.ap;
+  //       }
+  //       if (resources.hp) {
+  //         player.resources.hp = resources.hp;
+  //       }
+  //       if (resources.mp) {
+  //         player.resources.mp = resources.mp;
+  //       }
+  //     }
+  //     room.currentTurnPlayerName = player.name;
+  //   }
+  // }
+});
+
+socket.on("instance:player-action", (action) => {
+  // const enemies = gameServer.state.instance?.room?.enemies;
+  // const players = gameServer.state.instance?.room?.players;
+  // const attacker = players?.find((player) => player.id === action.attackerId);
+  // const target = enemies?.find((enemy) => enemy.id === action.targetId);
+  // if (target) {
+  //   displayDamagePopup(action.attackerId, action.targetId, action.damage, action.critical);
+  //   target.hp = Math.max(target.hp - action.damage, 0);
+  // }
+  // if (attacker) {
+  //   if (action.cost?.ap) {
+  //     attacker.resources.ap -= action.cost.ap;
+  //   }
+  //   if (action.cost?.mp) {
+  //     attacker.resources.mp -= action.cost.mp;
+  //   }
+  //   if (action.cost?.hp) {
+  //     attacker.resources.hp -= action.cost.hp;
+  //   }
+  // }
 });
